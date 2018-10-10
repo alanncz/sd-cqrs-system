d031d45674207e8595fd752680adaaa017743afb
alanncz <alannrodrigues@hotmail.com>
2018-10-10 12:29:10 -0300

    transformando objeto em transacional

--- postgres-app/src/main/java/com/natansevero/postgresapp/jdbc/DatabaseServiceImpl.java
+++ postgres-app/src/main/java/com/natansevero/postgresapp/jdbc/DatabaseServiceImpl.java
@@ -7,6 +7,7 @@ package com.natansevero.postgresapp.jdbc;
 
 import com.natansevero.shared.model.Usuario;
 import com.natansevero.shared.services.DatabaseService;
+import com.natansevero.shared.services.TxCoordinatorService;
 import java.rmi.RemoteException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,28 +21,38 @@ import java.util.logging.Logger;
  *
  * @author alann
  */
-public class DatabaseServiceImpl implements DatabaseService{
+public class DatabaseServiceImpl implements DatabaseService, TxCoordinatorService {
 
     private static final String URL = "jdbc:postgresql://database:5432/database";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
+    private boolean inTransaction;
+
+    public DatabaseServiceImpl() {
+        this.inTransaction = false;
+    }
 
     @Override
     public boolean inserir(Usuario usuario) throws RemoteException {
-        try {
-            Connection con =  DriverManager.getConnection(URL, USER, PASSWORD);
-            String sql = "insert into usuario (nome,imageurl,uuid) values(?, ?, ?)";
-            try (PreparedStatement ps = con.prepareStatement(sql)) {
-                ps.setString(1, usuario.getNome());
-                ps.setString(2, usuario.getImageUrl());
-                ps.setString(3, usuario.getUuid());
-                return true;
+        if (inTransaction) {
+            try {
+
+                Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+                String sql = "insert into usuario (nome,imageurl,uuid) values(?, ?, ?)";
+                try (PreparedStatement ps = con.prepareStatement(sql)) {
+                    ps.setString(1, usuario.getNome());
+                    ps.setString(2, usuario.getImageUrl());
+                    ps.setString(3, usuario.getUuid());
+                    return true;
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(DatabaseServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }   catch (SQLException ex) {
-            Logger.getLogger(DatabaseServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
+        else throw new RemoteException("Não deu pra inserir...");
         return false;
-    }  
+    }
 
     @Override
     public Usuario buscar(String uuid) throws RemoteException {
@@ -51,6 +62,25 @@ public class DatabaseServiceImpl implements DatabaseService{
     @Override
     public List<Usuario> buscarTodos() throws RemoteException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void prepare() throws RemoteException {
+        inTransaction = true;
+    }
+
+    @Override
+    public void roolback() throws RemoteException {
+        if (inTransaction) {
+            inTransaction = false;
+        }
+    }
+
+    @Override
+    public void commit() throws RemoteException {
+        if (inTransaction) {
+            inTransaction = false;
+        }
     }
 
 }

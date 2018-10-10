a03eb570f1d8053c6d8a566cea6aa707bee969cf
natansevero <natanseverolp@gmail.com>
2018-10-10 16:28:02 -0300

    Inserindo no mongodb

--- manager/src/main/java/com/natansevero/manager/services/ManagerServiceImpl.java
+++ manager/src/main/java/com/natansevero/manager/services/ManagerServiceImpl.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.natansevero.manager.main.services;
+package com.natansevero.manager.services;
 
 import com.natansevero.shared.model.Usuario;
 import com.natansevero.shared.services.DatabaseService;

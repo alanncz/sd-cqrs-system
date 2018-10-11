f4570028b2444716f1e58e3e826666d567fcc628
alanncz <alannrodrigues@hotmail.com>
2018-10-11 13:32:38 -0300

    finalizando cliente

--- client/src/main/java/com/natansevero/client/main/ClientApp.java
+++ client/src/main/java/com/natansevero/client/main/ClientApp.java
@@ -17,19 +17,28 @@ import javax.ws.rs.core.MediaType;
  * @author natan
  */
 public class ClientApp {
-    public static void main(String[] args) throws IOException  {
+    public static void main(String[] args) throws IOException, InterruptedException  {
         
         String name = "Alann";
         String imageLink = "https://www.comboinfinito.com.br/principal/wp-content/uploads/2017/04/goku-dragon-ball.jpg";
-        Usuario user = new Usuario();
-        user.setNome(name);
-        user.setImageUrl(imageLink);
-        Client client = Client.create(new DefaultClientConfig());
-        WebResource webResource = client.resource("http://localhost:8080/alannapp");
-        String texto = webResource.type(MediaType.APPLICATION_JSON).get(String.class);
-        System.out.println(texto);
-        webResource.type(MediaType.APPLICATION_XML).post(user);
-        System.in.read();
+        Usuario user1 = new Usuario();
+        user1.setNome(name);
+        user1.setImageUrl(imageLink);
+        
+        String nome2 = "Natan";
+        String imageLink2 = "https://i1.wp.com/ovicio.com.br/wp-content/uploads/vegeta-dragon-ball-super-995397.png?resize=663%2C373";
+        Usuario user2 = new Usuario();
+        user2.setNome(name);
+        user2.setImageUrl(imageLink);
+        
+        Thread t1 = new Thread(new WriteOperation(user1));
+        Thread t2 = new Thread(new WriteOperation(user2));
+        t1.start();
+        t2.start();
+        
+        Thread.sleep(2000); //apenas para teste de janela
+        Thread t3 = new Thread(new ReadOperation());
+        t3.start();
 
    }
 }
